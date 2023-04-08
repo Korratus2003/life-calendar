65f6a7ee66fa402a91273956661bf0594df96d12
Korratus2003 <95227592+Korratus2003@users.noreply.github.com>
2023-04-08 22:13:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const squaresContainer = document.getElementById('squares-container');
-const birthdateInput = document.getElementById('birthdate');
-birthdateInput.addEventListener('change', () => {
-    const birthdate = new Date(birthdateInput.value);
-    const currentDate = new Date();
-    const millisecondsInWeek = 1000 * 60 * 60 * 24 * 7;
-    const totalWeeks = 100 * 52;
-    const weeksLived = Math.floor((currentDate - birthdate) / millisecondsInWeek);
-    squaresContainer.innerHTML = '';
+const dateInput = document.getElementById("date-input");
+const squaresContainer = document.querySelector(".squares-container");
+
+dateInput.addEventListener("change", () => {
+    const birthDate = new Date(dateInput.value);
+    const now = new Date();
+
+    const diffInMilliseconds = now.getTime() - birthDate.getTime();
+    const diffInWeeks = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24 * 7));
+    const totalWeeks = Math.floor(100 * 52.143);
+
+    dateInput.style.display = "none";
+
     for (let i = 0; i < totalWeeks; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        if (i < weeksLived) {
-            square.classList.add('filled');
+        const square = document.createElement("div");
+        square.classList.add("square");
+        if (i < diffInWeeks) {
+            square.classList.add("filled");
         } else {
-            square.classList.add('empty');
+            square.classList.add("empty");
         }
         squaresContainer.appendChild(square);
     }
